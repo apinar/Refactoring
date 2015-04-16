55700839a92c1dd85c17d61ae99f39d002ef7f47
Alvaro <Alvaro@apinar>
2015-04-16 12:08:08 +0200

    Las nuevas clases deben extender a la clase Intervalo.

--- src/intervals/Both_opened.java
+++ src/intervals/Both_opened.java
@@ -1,5 +1,10 @@
 package intervals;
 
-public class Both_opened {
+public class Both_opened extends Interval{
+
+	public Both_opened(double minimum, double maximum, Opening opening) {
+		super(minimum, maximum, opening);
+		// TODO Auto-generated constructor stub
+	}
 
 }
--- src/intervals/Unopened.java
+++ src/intervals/Unopened.java
@@ -1,5 +1,10 @@
 package intervals;
 
-public class Unopened {
+public class Unopened extends Interval{
+
+	public Unopened(double minimum, double maximum, Opening opening) {
+		super(minimum, maximum, opening);
+		// TODO Auto-generated constructor stub
+	}
 
 }
